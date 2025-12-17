5d8c6d59028f4d8e14a6a792ad8ed6ca20c0dbe7
aminPersonal <amin.mohiuddin12@gmail.com>
2025-12-17 23:54:38 +0530

    largest rectangle

--- stacks/histogram.py
+++ stacks/histogram.py
@@ -1,22 +1,42 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         # max = heights[0]
+        # mA = 0
+        # stack = []
+        # length = len(heights)
+        # for i in range(length):
+        #     if stack:
+        #         stack.append((i, heights[i]))
+        #     elif stack and stack[i-1] < heights[i]:
+        #         stack.append((i, height[i]))
+        #     elif stack and  stack[i-1] > heights[i]:
+        #         j, h = stack.pop(i-1)
+        #         w = j-i
+        #         mA = max(w*h, mA)
+        # for i in range(len(stack), -1):
+        #     j, h = stack.pop(i)
+        #     w = length-j
+        #     mA = max((w*h), mA)        
+        # return mA
+
+
+        # full working solution
+        # def largestRectangleArea(self, heights: List[int]) -> int:
+        # max = heights[0]
         mA = 0
         stack = []
         length = len(heights)
-        for i in range(length):
-            if stack:
-                stack.append((i, heights[i]))
-            elif stack and stack[i-1] < heights[i]:
-                stack.append((i, height[i]))
-            elif stack and  stack[i-1] > heights[i]:
-                j, h = stack.pop(i-1)
-                w = j-i
-                mA = max(w*h, mA)
-        for i in range(len(stack), -1):
-            j, h = stack.pop(i)
-            w = length-j
-            mA = max((w*h), mA)        
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                mA = max(mA, height*(i-index))
+                start = index
+            stack.append((start, h))
+
+        for i, h in stack:
+            mA = max(mA, h*(len(heights) - i))
+
         return mA
 
 
